50d037bd49c047d0164d6597ae8739ad0c1b70f5
Arpit Narechania <arpitnarechania@gmail.com>
2026-09-16 09:09:23 +0800

    update server_url to render

--- server/public/main.js
+++ server/public/main.js
@@ -1826,7 +1826,8 @@ var SessionPage = /** @class */ (function () {
 }());
 
 var DeploymentConfig = Object.freeze({
-    SERVER_URL: "https://lumos-webapp-4aeadb3bf30d.herokuapp.com/"
+	SERVER_URL: "https://lumos-1stu.onrender.com/"
+    // SERVER_URL: "https://lumos-webapp-4aeadb3bf30d.herokuapp.com/"
     // SERVER_URL: "http://localhost:3000"
 });
 /**
