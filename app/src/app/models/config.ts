5fb4ea86f9cc0761dcc15878eda3f8a0558cbc97
LEONLOVE <leon459196355@gmail.com>
2026-09-22 16:41:26 +0800

    initial launch

--- app/src/app/models/config.ts
+++ app/src/app/models/config.ts
@@ -23,9 +23,9 @@ export class SessionPage {
 }
 
 export const DeploymentConfig = Object.freeze({
-  SERVER_URL: "https://lumos-1stu.onrender.com/",
+  // SERVER_URL: "https://lumos-1stu.onrender.com/",
   // SERVER_URL: "https://lumos-webapp-4aeadb3bf30d.herokuapp.com/",
-  // SERVER_URL: "http://localhost:3000"
+  SERVER_URL: "http://localhost:3000"
 });
 
 /**
